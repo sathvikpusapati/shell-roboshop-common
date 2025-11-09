f7ae4f54635895337029b297814fd62172f528dc
pusapati sathvik <pusapatisathvik@gmail.com>
2025-11-09 20:28:46 +0530

    common

--- redis.sh
+++ redis.sh
@@ -13,7 +13,7 @@ VALIDATE $? "ENABLING  REDIS 7"
 dnf install redis -y &>> $logfile
 VALIDATE $? "INSTALLING REDIS"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c /protected-mode no'  /etc/redis/redis.conf &>> $logfile
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no'  /etc/redis/redis.conf &>> $logfile
 VALIDATE $? "EDITING REDIS CONF FILE"
 
 systemctl enable redis &>> $logfile
