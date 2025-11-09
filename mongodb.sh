8622ecab87399a86096db0fa57b2afbf00cec44c
pusapati sathvik <pusapatisathvik@gmail.com>
2025-11-09 18:46:41 +0530

    common

---
+++ mongodb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root()
+
+cp  mongo.repo /etc/yum.repos.d/mongo.repo &>> $logfile
+
+VALIDATE $? "MONGO REPO COPIED"
+
+dnf install mongodb-org -y &>> $logfile
+VALIDATE $? "MONGODB INSTALLATION"
+
+systemctl enable mongod &>> $logfile
+VALIDATE $? "ENABLE MONGODB"
+
+systemctl start mongod &>> $logfile
+VALIDATE $? "START MONGODB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $logfile
+
+VALIDATE $? "Allowing remote connection to MONGODB" 
+
+systemctl restart mongod &>> $logfile
+VALIDATE $? "MONGODB RESTART" 
+
+print_total_time()
