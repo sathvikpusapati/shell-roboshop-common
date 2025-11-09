9760553ba7b6d4ae011f1dc31749f0a7001c5edb
pusapati sathvik <pusapatisathvik@gmail.com>
2025-11-09 19:56:27 +0530

    common

---
+++ redis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+dnf module disable redis -y &>> $logfile
+VALIDATE $? "DISABLING DEFAULT REDIS"
+
+dnf module enable redis:7 -y &>> $logfile
+VALIDATE $? "ENABLING  REDIS 7"
+
+dnf install redis -y &>> $logfile
+VALIDATE $? "INSTALLING REDIS"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c /protected-mode no'  /etc/redis/redis.conf &>> $logfile
+VALIDATE $? "EDITING REDIS CONF FILE"
+
+systemctl enable redis &>> $logfile
+VALIDATE $? "ENABLING  REDIS"
+
+systemctl start redis &>> $logfile
+VALIDATE $? "STARTING REDIS"
+
+print_total_time
